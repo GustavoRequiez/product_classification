b2185872db91d8774cf705e9fce886b15fcbb593
Gustavo Flores <gustavofc65@gmail.com>
2019-09-02 09:28:31 -0500

    [UPD] updated translation

--- models/product.py
+++ models/product.py
@@ -16,19 +16,3 @@ class ProductClassification(models.Model):
         ('X', 'X'),
         ('Y', 'Y'),
         ('Z', 'Z')], string="Classification XYZ")
-
-
-# class StockMoveLineClassification(models.Model):
-#     _inherit = 'stock.move.line'
-#     _name = 'stock.move.line'
-#
-#     line = fields.Boolean(
-#         string='Its Line', related='product_id.its_line')
-#     abc = fields.Selection(
-#         string='Classification ABC', related='product_id.classification_ABC')
-#     xyz = fields.Selection(
-#         string='Classification XYZ', related='product_id.classification_XYZ')
-#     categ = fields.Many2one(
-#         string='Category', related='product_id.categ_id')
-#     sellers = fields.One2many(
-#         string='Seller', related='product_id.seller_ids')

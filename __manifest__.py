696c3e52b85f1841ba8beb1a14ebf7be106af78e
gflores <gustavofc65@gmail.com>
2021-07-19 10:25:04 -0500

    [UPD] update version in manifest file

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,7 @@
     'author': "gflores",
     'website': "https://www.gruporequiez.com",
     'category': 'Inventory',
-    'version': '0.0.1',
+    'version': '14.0.0.0.1',
     'depends': ['product', 'stock'],
     'data': [
         # views
